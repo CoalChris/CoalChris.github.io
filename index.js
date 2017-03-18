fa18868ea40aa2b1da52b977b67c69c5936a11cc
Christopher Chan <coalchris@hotmail.com>
2017-03-18 15:50:17 +1100

    Template & Set-Up
    Starting off with a modified template and changing from there

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+   $(".jumbotron").hide();
+   $("#gallery-one").hide();
+   $("#gallery-two").hide();
+   $("#gallery-three").hide();
+   $("#gallery-four").hide();
+
+   $(".jumbotron").fadeIn(1000);
+   $("#gallery-one").fadeIn(1000);
+   $("#gallery-two").fadeIn(1000);
+   $("#gallery-three").fadeIn(1000);
+   $("#gallery-four").fadeIn(1000);
+});
