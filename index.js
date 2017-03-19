d1dc160dc48c205d9424e18ec5576e1f2e0b762d
Christopher Chan <coalchris@hotmail.com>
2017-03-19 18:45:59 +1100

    Concept change
    Homepage changed based on concept of site, also finished setting up
    navigation bar.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
    var figures = $("figure");
-
+   
    $(".jumbotron").hide();
    $.each(figures, function(i, item) {
       $(item).hide();
    })
    $("footer").hide();
    $("header").hide();
+   
 
    $("header").slideDown(800);
    $(".jumbotron").fadeIn(1000);
@@ -16,4 +17,15 @@ $(document).ready(function() {
       }, 800 * i);
    });
    $("footer").fadeIn(8000);
+
+   $("#searchbar").focus(function() {
+      $("#searchbtn").show(), 600, $(this).animate({
+         "width": "600px"
+      });
+   });
+   $("#searchbar").blur(function() {
+      $("#searchbtn").hide(), 600, $(this).animate({
+         "width": "200px"
+      });
+   });
 });
