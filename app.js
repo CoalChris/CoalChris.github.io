dd57c600ae50b121109441510873962c87027c06
Christopher Chan <coalchris@hotmail.com>
2017-04-13 01:08:18 +1000

    Added dynamic content updating
    Bugs still present for read more button, and unable to transition

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-var app = angular.module("TCT", ['ngSanitize']);
+var app = angular.module("TCT", ["ngSanitize", "ngAnimate"]);
--- stories.js
+++ stories.js
@@ -12,9 +12,34 @@ app.controller("stories", ["$scope", function($scope) {
          title: "SETTING UP HOME",
          image: "home.png",
          date: "6 April 2017",
-         tag: "website",
+         tag: "website technology",
          blurb: "The foundations of this website have begun development! Yay!",
          text: "Constructed from another conceptual webpage <a href=\"old.html\" target=\"_blank\">found here</a>."
       }
    ];
+
+   $scope.type = "all";
+   $scope.numResults = 0;
+   $scope.maxResults = 5;
+
+   $scope.filterType = function(story) {
+      if ($scope.numResults < $scope.maxResults) {
+         if ($scope.type == "all") {
+            $scope.numResults = $scope.numResults + 1;
+            return true;
+         } else {
+            if (story.tag.indexOf($scope.type) !== -1) {
+               $scope.numResults = $scope.numResults + 1;
+               return true;
+            }
+         }
+      }
+      return false;
+   }
+
+   $scope.filter = function(value) {
+      $scope.type = value;
+      $scope.numResults = 0;
+      $scope.$apply();
+   }
 }]);
